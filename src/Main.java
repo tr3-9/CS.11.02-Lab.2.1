e5b2f1c6a9c2d1296dfd8b7383821b2d504bd362
tr3-9 <tonyr2027@student.cis.edu.hk>
2024-09-08 21:52:10 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,51 @@
 /**
-*****************************************************************************************************
-* By writing your name in the space provided below, you are honouring the CIS academic honesty pledge 
-* “I have neither given nor received unauthorized aid on this piece of work.”
-* NAME: 
-*****************************************************************************************************
-*/
+ *****************************************************************************************************
+ * By writing your name in the space provided below, you are honouring the CIS academic honesty pledge
+ * “I have neither given nor received unauthorized aid on this piece of work.”
+ * NAME:
+ *****************************************************************************************************
+ */
 
 public class Main {
 
     public static void main(String[] args) {
 
     }
+    public static int add(int a, int b) {
+        return a + b;
+    }
 
-    // 1. add
-    
-    // 2. add
+    public static int add(int a, int b, int c, int d) {
+        int firstSum = add(a, b);
+        int secondSum = add(c, d);
+        return add(firstSum, secondSum);
+    }
 
-    // 3. morningGreeting
+    public static String greeting(String name) {
+        return "早上好， " + name;
+    }
 
-    // 4. afternoonGreeting
+    public static String afternoonGreeting(String name) {
+        return "下午好" + name;
+    }
 
-    // 5. triple
+    public static String triple(String name) {
+        return name + name + name;
+    }
 
-    // 6. half
+    public static int half(int a) {
+        return a/2;
+    }
 
-    // 7. roundPositiveValueToNearestInteger
+    public static int roundPositiveValueToNearestInteger(double value) {
+        return (int) Math.round(value);
+    }
 
-    // 8. roundNegativeValueToNearestInteger
 
 }
+
+
+
+
+
+
